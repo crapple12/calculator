741b0a3458243956d85a8996300f0a88be31763d
crapple12 <enoestephon@gmail.com>
2024-09-27 12:09:19 -0400

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+
+function add (num1, num2) {
+   let sum = num1 + num2;
+   return sum;
+}
+
+function subtract (num1, num2) {
+    let dif = num1 - num2;
+    return dif;
+}
+
+function multiply (num1, num2) {
+    let product = num1 * num2;
+    return product;
+}
+
+function divide (num1, num2) {
+    let divide = num1 % num2;
+    return divide;
+}
