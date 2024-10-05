a8116f3e682f7607a392e501770416b788e7b189
crapple12 <enoestephon@gmail.com>
2024-10-05 10:58:04 -0400

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,20 +1,15 @@
+let operator = "";
+let previousValue = "";
+let currentValue = "";
 
-function add (num1, num2) {
-   let sum = num1 + num2;
-   return sum;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let clear = document.querySelector("clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
 
-function subtract (num1, num2) {
-    let dif = num1 - num2;
-    return dif;
-}
+    let numbers = document.querySelector(".number");
+    let operators = document.querySelector(".operator");
 
-function multiply (num1, num2) {
-    let product = num1 * num2;
-    return product;
-}
-
-function divide (num1, num2) {
-    let divide = num1 % num2;
-    return divide;
-}
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+})
